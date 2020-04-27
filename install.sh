382e14a114c82637705f47f0215f63e17552d1ad
Gabor Csardi <csardi.gabor@gmail.com>
2020-04-27 08:37:39 +0100

    Simplify install with our csw repo
    Also add sysreqs install and TeX

--- install.sh
+++ install.sh
@@ -13,100 +13,36 @@ else
     case ${0##*/} in sh|dash) sourced=1;; esac
 fi
 
-function install_tools() {
-    /opt/csw/bin/pkgutil -y -i curl gcc5core gcc5gfortran gcc5g++ \
-			 libreadline_dev libiconv_dev libz_dev liblzma_dev \
-			 libpcre_dev libcurl_dev libssl_dev libssh2_dev \
-			 libcares_dev openldap_dev libbrotli_dev \
-			 libkrb5_dev libcairo_dev librtmp_dev gmake gtar
+function install_r_opencsw() {
+    echo "== INSTALLING R ==================================="
+    sudo pkgutil -y -i libreadline7
+    sudo pkgutil -t https://files.r-hub.io/opencsw -y -i r_base
 }
 
-function download_r_devel {
-    /opt/csw/bin/curl -O https://stat.ethz.ch/R/daily/R-devel.tar.gz
-    gzip -dc R-devel.tar.gz | tar xf -
-}
-
-function download_r_patched {
-    /opt/csw/bin/curl -O https://stat.ethz.ch/R/daily/R-patched.tar.gz
-    gzip -dc R-patched.tar.gz | tar xf -
-}
-
-function compile_r_patched {
+function install_sysreqs() {
     (
 	set -e
-	if [ -e /opt/R/R-patched ]; then
-	    >&2 echo /opt/R/R-patched already exists, exiting
-	    exit 1
-	fi
-	cd R-patched
-	if [ ! -e config.site.orig ]; then
-	    mv config.site config.site.orig
-	fi
-	cp ../config.site .
-	export LD_LIBRARY_PATH=/opt/csw/lib
-	export PKG_CONFIG_PATH=/opt/csw/lib/pkgconfig
-	export PATH=$PATH:/usr/ccs/bin
-	export MAKE=/opt/csw/bin/gmake
-	export TAR=/opt/csw/bin/gtar
-	./configure -C --with-internal-tzcode \
-		    --prefix=/opt/R/R-patched
-	/opt/csw/bin/gmake
-	/opt/csw/bin/gmake install
+	tools=$(cat sysreqs.txt)
+	sudo pkgutil -y -i $tools
     )
 }
 
-function cleanup_r_patched() {
-    rm -rf R-patched*
-}
-
-function compile_r_devel {
-    (
-	set -e
-	if [ -e /opt/R/R-devel ]; then
-	    >&2 echo /opt/R/R-devel already exists, exiting
-	    exit 1
-	fi
-	cd R-devel
-	if [ ! -e config.site.orig ]; then
-	    mv config.site config.site.orig
-	fi
-	cp ../config.site .
-	export LD_LIBRARY_PATH=/opt/csw/lib
-	export PKG_CONFIG_PATH=/opt/csw/lib/pkgconfig
-	export PATH=$PATH:/usr/ccs/bin
-	export MAKE=/opt/csw/bin/gmake
-	export TAR=/opt/csw/bin/gtar
-	./configure -C --with-internal-tzcode --with-pcre1 \
-		    --prefix=/opt/R/R-devel
-	/opt/csw/bin/gmake
-	/opt/csw/bin/gmake install
-    )
-}
-
-function cleanup_r_devel() {
-    rm -rf R-devel*
-}
-
-function cleanup() {
-    cleanup_r_patched
-    cleanup_r_devel
+function install_tex() {
+    sudo pkgutil -y -i \
+	 texlive_base \
+	 texlive_binaries \
+	 texlive_common \
+	 texlive_fonts_extra \
+	 texlive_fonts_recommended \
+	 texlive_latex_base \
+	 texlive_latex_base_binaries \
+	 texlive_latex_extra_binaries
 }
 
 function main() {
-    echo "== INSTALLING BUILD TOOLS ================="
-    install_tools
-
-    echo "== DOWNLOADING R-devel ===================="
-    download_r_devel
-
-    echo "== COMPILING R-devel ======================"
-    compile_r_devel
-
-    echo "== DOWNLOADING R-patched =================="
-    download_r_patched
-
-    echo "== COMPILING R-partched ==================="
-    compile_r_patched
+    install_r_opencsw
+    install_sysreqs
+    install_tex
 }
 
 if [ "$sourced" = "0" ]; then
